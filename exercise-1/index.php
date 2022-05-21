e4dc2107ac9f1b170c66c70ec14c3d260debf9b6
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-05-21 06:57:26 -0700

    Add index.php

---
+++ exercise-1/index.php
@@ -0,0 +1,44 @@
+<?php include 'inc/header.php' ?>
+
+<?php include 'utils/logger.php' ?>
+
+<?php
+    $message;
+
+    if (isset($_POST['submit'])) {
+        // get the input value
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        if ($name === '') {
+            $message = 'Your name is required';
+        }
+    }
+?>
+
+<form class="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input autocomplete="off" type="text" name="name" id="name" placeholder="Please enter your name">
+        <br />
+        <?php if (isset($message)): ?>
+            <span>
+                <?php echo $message ?>
+            </span>
+        <?php endif; ?>
+    </div>
+    <div class="form-group">
+        <button name="submit" type="submit">
+            Submit
+        </button>
+    </div>
+</form>
+
+<?php if (isset($name) && $name !== ''): ?>
+    <div class="message">
+        <p>
+            <?php echo 'Hello' . ' ' . $name ?>
+        </p>
+    </div>
+<?php endif; ?>
+
+<?php include 'inc/footer.php' ?>
