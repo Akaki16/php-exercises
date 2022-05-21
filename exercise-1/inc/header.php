55950d211655baa66802ed6c8358633c47eb047c
Akaki Burjanadze <akakiburjanadze17@gmail.com>
2022-05-21 12:28:10 -0700

    Change page title

--- exercise-1/inc/header.php
+++ exercise-1/inc/header.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Greeting Program</title>
+    <title>Greeting Webpage</title>
     <style>
         <?php include 'css/styles.css' ?>
     </style>
